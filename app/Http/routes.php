a97a4752f062ae9efaab77348ed371ac46caa399
Jasana Mael <jasanaemily@hotmail.com>
2016-02-03 15:25:08 +1300

    Set up view files and added some blade code

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,19 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('about', function(){
+	return view('about');
+});
+
+Route::get('contact', function(){
+	return view('contact');
+});
+
+Route::get('product', function(){
+	return view('product');
 });
 
 /*
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('master')
+
+@section('content')
+<h1>Home</h1>
+
+@endsection
